0e19c6b6ca2bcadff3cc69b0f20e230f03ad2816
Suresh Kumar <ft24-bscs-0022@qu.edu.pk>
2024-10-07 15:31:50 +0500

    Add files via upload

---
+++ KeyLogger/script.js
@@ -0,0 +1,39 @@
+const logDiv =document.getElementById("log"); 
+const stateDiv = document.getElementById("state"); 
+
+const startBtn = document.getElementById("start-btn");
+
+const stopBtn = document.getElementById("stop-btn"); 
+
+
+
+startBtn.addEventListener("click",()=> {
+    document.addEventListener("keydown", handleDown);   
+    document.addEventListener("keyup", handleUp);
+
+
+
+});
+
+
+stopBtn.addEventListener("click",()=> {
+    document.addEventListener("keydown", handleDown);   
+    document.addEventListener("keyup", handleUp);
+    logDiv.textContent = "";
+    stateDiv.textContent= " "
+
+
+    
+});
+
+function handleDown(e) {
+    logDiv.textContent = `key ${e.key} Pressed Down`; 
+    stateDiv.textContent = 'key is down';
+}
+
+function handleUp(e) {
+    logDiv.textContent = `key ${e.key} Pressed Up`; 
+    stateDiv.textContent = 'key is up';
+}
+
+
